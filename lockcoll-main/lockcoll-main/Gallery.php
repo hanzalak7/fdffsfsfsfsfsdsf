1433bcaa1e5a672403f7d74aa0e38ec5c718bb90
Hanzala Kathewadi <69789320+hanzalak7@users.noreply.github.com>
2023-04-07 00:03:00 +0530

    Add files via upload

---
+++ lockcoll-main/lockcoll-main/Gallery.php
@@ -0,0 +1,229 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <style>
+        .gallery {
+          border: 1px solid #ccc;
+        }
+        
+        .gallery:hover {
+          border: 1px solid #777;
+        }
+        
+        .gallery img {
+          width: 100%;
+          height: auto;
+        }
+        
+        .desc {
+          padding: 15px;
+          text-align: center;
+        }
+        
+        * {
+          box-sizing: border-box;
+        }
+        
+        .responsive {
+          padding: 0 6px;
+          float: left;
+          width: 24.99999%;
+        }
+        
+        @media only screen and (max-width: 700px) {
+          .responsive {
+            width: 49.99999%;
+            margin: 6px 0;
+          }
+        }
+        
+        @media only screen and (max-width: 500px) {
+          .responsive {
+            width: 100%;
+          }
+        }
+        
+        .clearfix:after {
+          content: "";
+          display: table;
+          clear: both;
+        }
+        </style>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NLJRPCSM1S"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NLJRPCSM1S');
+        <!-- Google tag (gtag.js) -->
+</script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name='keywords' content='Lokmanya College' />
+    <meta name='description' content='' />
+
+    <title>Welcome Lokmanya College</title>
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/vendor/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/vendor/aos/aos.css">
+    <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.6.12/dist/css/splide.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@600;700&family=Ubuntu:wght@400;500&display=swap"
+        rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="assets/images/favicon.png">
+
+</head>
+
+<body>
+
+
+    <!-- ======= Start Header ======= -->
+    <?php include './reuse-com/header.php';?>   
+    <!-- ======= end Header ============ -->
+    <!-- ======= Start Section 1 ======= -->
+
+    <div class="Contaier">
+        <br>
+        <div class="gallery">
+
+            <img src="assets/images/slider/slide1.jpg" alt="#" width="600" height="400">
+            </a>
+
+        </div>
+        <br>
+        <div class="gallery">
+
+            <img src="assets/images/slider/slide2.jpg" alt="#" width="600" height="400">
+            </a>
+
+        </div>
+        <br>
+        <div class="gallery">
+
+            <img src="assets/images/slider/slide3.jpg" alt="#" width="600" height="400">
+            </a>
+
+        </div>
+        <br>
+        <div class="gallery">
+
+            <img src="assets/images/slider/slide4.jpg" alt="#" width="600" height="400">
+            </a>
+
+        </div><br>
+        <div class="gallery">
+
+            <img src="assets/images/slider/slide5.jpg" alt="#" width="600" height="400">
+            </a>
+
+        </div><br>
+        <div class="gallery">
+
+            <img src="assets/images/slider/slide6.jpg" alt="#" width="600" height="400">
+            </a>
+
+        </div><br>
+        <div class="gallery">
+
+            <img src="assets/images/slider/slide7.jpg" alt="#" width="600" height="400">
+            </a>
+
+        </div><br>
+        <div class="gallery">
+
+            <img src="assets/images/slider/slide8.jpg" alt="#" width="600" height="400">
+            </a>
+
+        </div><br>
+        <div class="gallery">
+
+            <img src="assets/images/slider/slide9.jpg" alt="#" width="600" height="400">
+            </a>
+
+        </div><br>
+        <div class="gallery">
+
+            <img src="assets/images/slider/slide10.jpg" alt="#" width="600" height="400">
+            </a>
+
+        </div><br>
+        <div class="gallery">
+
+            <img src="assets/images/slider/slide11.jpg" alt="#" width="600" height="400">
+            </a>
+
+        </div><br>
+        <div class="gallery">
+
+            <img src="assets/images/slider/slide12.jpg" alt="#" width="600" height="400">
+            </a>
+
+        </div><br>
+        <div class="gallery">
+
+            <img src="assets/images/slider/slide13.jpg" alt="#" width="600" height="400">
+            </a>
+
+        </div>
+    </div>
+    <br><br>
+    <!-- ======= end Section 4  ======== -->
+    <!-- ======= Start Section 5 ======= -->
+
+    <!-- ======= end Section 5  ============= -->
+    <!-- ======= Slider Section ============= -->
+
+    <!-- ===== End Slider Section========== -->
+
+    <!-- ======= Start Footer Section ======= -->
+    <?php include './reuse-com/footer.php';?>   
+    <!-- ======= end Footer Section ======= -->
+
+    <!-- ======= JavaScript links  ======== -->
+    <script src="assets/js/main.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+        integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
